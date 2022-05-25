c59ee19697b855739a533377c2c8e6fb81eba927
Bug Norris <no@e.mail>
2022-05-25 10:13:12 +0200

    added important feature

---
+++ game/scenes/Game.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	return OS.shell_open('https://youtu.be/dQw4w9WgXcQ')
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
