07dee5da4869dc1d3563042377a33b20291f9e60
Voriali <49492450+heriom@users.noreply.github.com>
2022-06-05 14:47:25 +0200

    Graphics and Sounds + Fixing Randomize Bug
    fixed the randomize bug with the help of a script own RandomNumberGenerator
    
    added some new graphics. background and new walls, yes they look pretty like the normal flappy bird but are still changable.
    
    the game now has sounds and i added a sound when the player dies for later when the gamemanager handles the end of a game better
    
    now the game starts with the press of a button and i changed how the game handles the start of a game

--- game/games/flappybird/Player.gd
+++ game/games/flappybird/Player.gd
@@ -4,48 +4,78 @@ const UP = Vector2(0, -1)
 const FLAP_HEIGHT = 200  # flap time
 const MAXFALLSPEED = 200
 const GRAVITY = 10
-const END_MESSAGE := "You got %s points!"
+const END_MESSAGE := "You got %s point(s)!"
 
 var timer = 0  # timer; spawning walls
 
 var motion = Vector2()
 var wall = preload("res://games/flappybird/WallNode.tscn")
 var score = 0
+var gameRunning = false
 
+onready var _score_label := $"../../CanvasLayer/RichTextLabel"
+onready var _rng := RandomNumberGenerator.new()
 
 func ready():
 	pass
 
 
+# called every frame
 func _physics_process(delta):
-	timer += delta
+	
+	# handle game start
+	if not gameRunning and Input.is_action_just_pressed("flap"):
+		start_game()
+	
+	# handle player physics behaviour
+	if gameRunning:
+		addToTimer(delta)
 
-	if timer >= 1:
-		timer = 0
-		spawn_wall()
+		motion.y += GRAVITY
+		if motion.y > MAXFALLSPEED:
+			motion.y = MAXFALLSPEED
 
-	motion.y += GRAVITY
-	if motion.y > MAXFALLSPEED:
-		motion.y = MAXFALLSPEED
-
-	if Input.is_action_just_pressed("flap"):
-		if position.y > -80:
-			motion.y = -FLAP_HEIGHT
-
+		if Input.is_action_just_pressed("flap"):
+			flap()
+	
+	
 	if position.y > 130:
 		GameManager.end_game(END_MESSAGE % score)
 		return
 
 	motion = move_and_slide(delta * 60 * motion, UP)
 
-	get_parent().get_parent().get_node("CanvasLayer/RichTextLabel").text = str(score)
+	_score_label.text = str(score)
 
 
-# spawn a new wall at the right side of the screen
+# start the game
+func start_game():
+	get_parent().get_parent().get_node("CanvasLayer/RichTextLabel2").visible = false
+	_rng.randomize()
+	gameRunning = true
+
+
+# flap when player presses ,,space"
+func flap():
+	if position.y > -80:
+		motion.y = -FLAP_HEIGHT
+		$Sound_Jump.play()
+
+
+# add delta to the timer
+func addToTimer(delta):
+	timer += delta
+	
+	if timer >= 1.2:
+		timer = 0
+		spawn_wall()
+
+
+# spawn a new wall when timer hits 1.2
 func spawn_wall():
 	var instance = wall.instance()
-	rand_seed(rand_range(0, 25))
-	instance.position = Vector2(500, rand_range(-60, 60))
+	
+	instance.position = Vector2(500, _rng.randi_range(-60, 60))
 	get_parent().call_deferred("add_child", instance)
 
 
@@ -59,10 +89,11 @@ func _on_Reset_body_entered(body):
 func _on_Hitbox_area_entered(area):
 	if area.name == "PointHitbox":
 		score += 1
-		print(score)
+		$Sound_Wall.play()
 
 
 # reload scene if player hits wall body
 func _on_Hitbox_body_entered(body):
 	if body.name == "Wall":
+		# here would come a death sound when player dies (which is already fully implemented ($Sound_GameEnd)) / when the gamemanager is evolved enough to handle that, ill add it
 		GameManager.end_game(END_MESSAGE % score)
