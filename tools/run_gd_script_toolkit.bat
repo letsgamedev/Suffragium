9275b73f5ff7096f483c0938f08dfb050044e4f1
Numenter <84922125+Numenter@users.noreply.github.com>
2022-06-22 13:52:05 +0200

    Update run_gd_script_toolkit.bat

--- tools/run_gd_script_toolkit.bat
+++ tools/run_gd_script_toolkit.bat
@@ -1,4 +1,15 @@
+@title Run GDScript Toolkit
+@echo off
+rem *********************
+rem This Script runs gdformat and gdlint on all files in game folder.
+rem https://github.com/Scony/godot-gdscript-toolkit
+rem *********************
 cd ..
+echo --- Format ---
 gdformat game/
+echo.
+echo --- Lint ---
 gdlint game/
+echo.
+echo.
 pause
